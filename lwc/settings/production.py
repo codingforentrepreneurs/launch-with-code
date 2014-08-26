437cabf26109de70346c51f46e75ff29487e26d9
Justin Mitchel <codingforentrepreneurs@gmail.com>
2014-08-25 17:34:17 -0700

    Add Requirements

---
+++ lwc/settings/production.py
@@ -0,0 +1,27 @@
+import os
+from django.conf imporet settings
+
+DEBUG = False
+TEMPLATE_DEBUG = True
+
+DATABASES = settings.DATABASES
+
+# Parse database configuration from $DATABASE_URL
+import dj_database_url
+DATABASES['default'] =  dj_database_url.config()
+
+# Honor the 'X-Forwarded-Proto' header for request.is_secure()
+SECURE_PROXY_SSL_HEADER = ('HTTP_X_FORWARDED_PROTO', 'https')
+
+# Allow all host headers
+ALLOWED_HOSTS = ['*']
+
+# Static asset configuration
+# import os
+# BASE_DIR = os.path.dirname(os.path.abspath(__file__))
+# STATIC_ROOT = 'staticfiles'
+# STATIC_URL = '/static/'
+
+# STATICFILES_DIRS = (
+#     os.path.join(BASE_DIR, 'static'),
+# )
--- lwc/wsgi.py
+++ lwc/wsgi.py
@@ -12,3 +12,10 @@ os.environ.setdefault("DJANGO_SETTINGS_MODULE", "lwc.settings")
 
 from django.core.wsgi import get_wsgi_application
 application = get_wsgi_application()
+
+try:
+	from dj_static import Cling
+
+	application = Cling(get_wsgi_application())
+except:
+	pass
